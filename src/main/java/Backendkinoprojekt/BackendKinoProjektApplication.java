957e99e2e7c2a44f36153080981e4bca95f19717
kris52h6 <kris52h6@stud.kea.dk>
2022-10-03 12:25:57 +0200

    General folder structure

--- src/main/java/Backendkinoprojekt/BackendKinoProjektApplication.java
+++ src/main/java/Backendkinoprojekt/BackendKinoProjektApplication.java
@@ -1,4 +1,4 @@
-package com.example.backendkinoprojekt;
+package Backendkinoprojekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
