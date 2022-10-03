5b61dee788e2e2e1b35128ca3f5baa07a20b43ad
Mark <markstone1234@hotmail.com>
2022-10-03 09:34:54 +0200

    Init

---
+++ src/main/java/com/example/backendkinoprojekt/BackendKinoProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendkinoprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKinoProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKinoProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendkinoprojekt/BackendKinoProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendkinoprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKinoProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
