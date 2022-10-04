174a4255beb8c5947caa80b093b0eccc5e7aa634
alexander6273 <alex6273@stud.kea.dk>
2022-10-04 11:02:39 +0200

    Theater test getTheater Reviewed-by: Oliver

---
+++ src/test/java/Backendkinoprojekt/service/TheaterGetById.java
@@ -0,0 +1,13 @@
+package Backendkinoprojekt.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TheaterGetById {
+
+    @Test
+    void getTheater()
+    {
+
+    }
+}
