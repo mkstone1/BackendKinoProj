cc78f99c4e541557e3779bb66daca49a263f1c9f
Mark <markstone1234@hotmail.com>
2022-10-04 09:46:41 +0200

    Movie entity og setup data klasse

---
+++ src/main/java/Backendkinoprojekt/configuration/SetupDevData.java
@@ -0,0 +1,4 @@
+package Backendkinoprojekt.configuration;
+
+public class SetupDevData {
+}
---
+++ src/main/java/Backendkinoprojekt/entity/Movie.java
@@ -0,0 +1,39 @@
+package Backendkinoprojekt.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+
+    private String name;
+
+    private String genre;
+
+    private int minAge;
+
+    private String actors;
+
+    private double runTime;
+
+
+
+}
