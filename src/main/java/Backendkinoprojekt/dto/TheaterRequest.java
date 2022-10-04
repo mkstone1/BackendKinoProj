f944e528dfe7c285669026fc03598ce163a35090
alexander6273 <alex6273@stud.kea.dk>
2022-10-04 10:03:35 +0200

    Theater request created and typo fixed Reviewed-by: Oliver

---
+++ src/main/java/Backendkinoprojekt/dto/TheaterRequest.java
@@ -0,0 +1,28 @@
+package Backendkinoprojekt.dto;
+
+import Backendkinoprojekt.entity.Theater;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TheaterRequest
+{
+    int id;
+    int rows;
+    int seatsPrRow;
+
+    public static Theater getTheaterEntity(TheaterRequest theaterRequest){
+        return new Theater(
+                theaterRequest.getId(),
+                theaterRequest.getRows(),
+                theaterRequest.getSeatsPrRow()
+        );
+    }
+}
